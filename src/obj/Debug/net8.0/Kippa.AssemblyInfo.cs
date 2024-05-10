90894f21e46c521e618b137c6a7ceeb07fc86d83
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-05-09 22:41:48 -0300

    add config obj

--- src/obj/Debug/net8.0/Kippa.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/Kippa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4adc485c2bed4b8da92d9870f69a4fb029dc2ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb95b44b21b254ac639480b4576720a7fd7cf7ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
