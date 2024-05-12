47378db6c88aa584202253b1cf00bcbb12398017
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-05-12 09:10:38 -0300

    atualização da controller usuarios: adição do metodo login para validação de senha do usuario

--- src/Controllers/UsuariosController.cs
+++ src/Controllers/UsuariosController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Kippa.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Kippa.Controllers
 {
@@ -24,6 +26,49 @@ namespace Kippa.Controllers
             return View(await _context.Usuarios.ToListAsync());
         }
 
+        public async Task<IActionResult> FormLogin()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Login(Usuario usuario)
+        {
+            var dados =  _context.Usuarios.FirstOrDefault(u => u.Nome == usuario.Nome);
+            if (dados == null)
+            {
+               return ViewBag.Message = "Usuario ou senha estão inválidos";
+            }
+            bool senhaOK = BCrypt.Net.BCrypt.Verify(usuario.Senha, dados.Senha);
+            if (senhaOK)
+            {
+                // Claim são informações credenciais do usuário
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, dados.Nome),
+                    new Claim(ClaimTypes.NameIdentifier, dados.IdUsuario.ToString()),
+                    new Claim(ClaimTypes.Name, dados.Nome)
+
+                };
+
+                var usuarioIdentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(usuarioIdentity);
+
+                //define quano tempo vai expirar a senha do usuário
+                var props = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTime.UtcNow.ToLocalTime().AddHours(8),
+                    IsPersistent = true
+                };
+                await HttpContext.SignInAsync(principal, props);
+
+                Redirect("/");
+            }
+            else
+            {
+                ViewBag.Message = "Usuario ou senha estão inválidos";
+            }
+            return RedirectToAction("Index");
+        }
         // GET: Usuarios/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -57,6 +102,7 @@ namespace Kippa.Controllers
         {
             if (ModelState.IsValid)
             {
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -96,6 +142,7 @@ namespace Kippa.Controllers
             {
                 try
                 {
+                    usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                     _context.Update(usuario);
                     await _context.SaveChangesAsync();
                 }
