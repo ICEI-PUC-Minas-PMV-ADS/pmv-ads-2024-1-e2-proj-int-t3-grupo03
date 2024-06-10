138f4f035c589cd189164da627be634ded7ed2d6
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-06-09 22:18:01 -0300

    atualizar configurações para publicar no git actions

--- src/obj/Debug/net8.0/Kippa.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/Kippa.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5eb54efe47580be502f84b8ba4de8787c3a5e72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e891e6684520a7ac7f883cd3cb49098321f0db9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kippa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
